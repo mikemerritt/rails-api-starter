668e2c7c726218d46ade6316528f7ca9c9975d31
Mike Merritt <emmjemm@gmail.com>
2017-03-24 16:43:13 -0500

    change cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins 'localhost:3500'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
